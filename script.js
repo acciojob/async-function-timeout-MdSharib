c8978d806dcf06b25485aa699d52fdd9cad9a302
Md Sharib <109461910+MdSharib@users.noreply.github.com>
2023-06-30 12:30:20 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,38 @@
 //your JS code here. If required.
+
+const btn = document.getElementById("btn");
+
+
+
+const delayFunction = (userText, userDelay) => {
+  return new Promise((res, rej) => {
+  setTimeout(() => {
+    res(userText);
+    }, +userDelay*1000)
+  });
+  
+};
+
+async function display(userText, userDelay){
+  const toDisplay = await delayFunction(userText, userDelay);
+  console.log(toDisplay);
+}
+
+
+
+
+
+
+const btnHandler = (ev) => {
+  ev.preventDefault();
+  let userText = document.getElementById("text").value;
+  let userDelay = document.getElementById("delay").value;
+  display(userText, userDelay);
+}
+
+
+
+
+
+btn.addEventListener('click', btnHandler);
+
